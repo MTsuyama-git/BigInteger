027dd9a9b52f4f70f78107fde0fe025c1c2008fe
MTsuyama-git <miyabi-yamabiko@hotmail.co.jp>
2023-03-23 19:01:52 +0900

    added build task

--- test/test_BigInteger.cpp
+++ test/test_BigInteger.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-    BigInteger bi(65535, Endian::Endian_Big);
+    BigInteger bi(65535, Endian::Endian_Little);
     std::cout << bi << std::endl;
 
     return 0;
