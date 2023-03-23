9ef1c52869f5e83102d1a64fb04b7c6ab35b1d08
MTsuyama-git <miyabi-yamabiko@hotmail.co.jp>
2023-03-23 18:56:01 +0900

    added build scripts

---
+++ scripts/cmake/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+CWD=$(dirname $(dirname $(cd $(dirname $0) && pwd)))
+cd ${CWD}
+if [ "$1" = "Debug" ];then
+        DEST=build.dbg
+        BuildType=Debug
+    else
+        DEST=build
+        BuildType=Release
+    fi
+if ["$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT']; then
+    DEST=build    
+    cmake -S . -B ${DEST}
+    cmake --build ${DEST} --config ${BuildType}
+else    
+    cmake -S . -G Ninja -B ${DEST} -DCMAKE_BUILD_TYPE=${BuildType}
+    cmake --build ${DEST}
+fi
+
+
